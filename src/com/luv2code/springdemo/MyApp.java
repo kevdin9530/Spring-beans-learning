02d97b5e396f1f396adada0ca9b292a0f898f23f
Kevin Dinh <kevdin9530@gmail.com>
2021-01-14 16:00:23 +0100

    Learn about Spring Beans/Container and configuration
    Added beans to configuration file and MyLoggerConfig to show additional
    log data

--- src/com/luv2code/springdemo/MyApp.java
+++ src/com/luv2code/springdemo/MyApp.java
@@ -1,4 +1,13 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp {
-
+	public static void main(String [] args ) {
+		//create object
+		Coach bbc = new TrackCoach();
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//use the object
+		System.out.print(bbc.getDailyWorkout());
+	}
 }
--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -1,4 +1,11 @@
+package com.luv2code.springdemo;
 
-public class TrackCoach {
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Run boi run";
+	}
 
 }
