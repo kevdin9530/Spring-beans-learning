ec3aaee79b0e751b6a793001d1e084ec9ce93759
Kevin Dinh <kevdin9530@gmail.com>
2021-01-14 16:21:24 +0100

    Testing SSH push

---
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,19 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+	public static void main(String[]args) {
+		//load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		
+		//retrieve the beans from container
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+		
+		//call method on beans
+		System.out.print(theCoach.getDailyWorkout());
+		//	close the context
+		context.close();
+	}
+}
